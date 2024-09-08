a19b7fa239702c7a33025d0d270031b58aee75b8
NGYY14 <panupongphosri2540@gmail.com>
2024-09-08 15:11:58 +0700

    8-9-667

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,76 +1,89 @@
 <template>
-  <div>
-    <header class="header-container">
-      <div class="header-content">
-        <h1 class="header-title">Happy Birthday</h1>
-      </div>
-    </header>
-    <div class="nav-wrapper">
-      <nav class="nav-links">
-        <nuxt-link to="/" class="nav-link">Home</nuxt-link>
-        <nuxt-link to="/about" class="nav-link">About</nuxt-link>
-        <nuxt-link to="/account" class="nav-link">Account</nuxt-link>
-        <nuxt-link to="/themes" class="nav-link">Themes</nuxt-link>
-        <nuxt-link to="/contact" class="nav-link">Contact Us</nuxt-link>
-        <nuxt-link to="/contact" class="nav-link">Contact Us</nuxt-link>
-        <nuxt-link to="/contact" class="nav-link">Contact Us</nuxt-link>
-        <nuxt-link to="/contact" class="nav-link">Contact Us</nuxt-link>
-      </nav>
+  <div class=" h-screen w-[430px]  ">
+    <div class="h-[30px] border-[1px] flex items-center gap-2">
+      <div class="w-[20px] h-[20px] rounded-full bg-orange-300"></div>
+      <div class="w-[20px] h-[20px] rounded-full bg-orange-300"></div>
+      <div class="w-[20px] h-[20px] rounded-full bg-orange-300"></div>
     </div>
+    <div class="flex flex-col items-center justify-center pt-5 ">
+      <div class="day  text-[24px]">Happy</div>
+      <div class="day  text-[40px]">Birthday</div>
+    </div>
+    <div class="flex justify-around pt-7 z-1">
+      <div>
+        <img class="w-[90px] h-[80px] animate-bounce" src="/img/Rectangle 162.png" alt="">
+        <img class="w-[90px] h-[80px] " src="/img/Group 5.png" alt="">
+      </div>
+
+      <div class="h-[300px] w-[150px] border-[2px]"></div>
+
+      <div>
+        <img class="w-[90px] h-[80px] animate-bounce" src="/img/Rectangle 162.png" alt="">
+        <img class="w-[90px] h-[80px]" src="/img/Group 5.png" alt="">
+      </div>
+    </div>
+    <div class="overflow-auto  flex gap-3  snap-x pt-4 ">
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-centerv"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+      <img class="w-[200px] h-[150px] snap-center"
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+    </div>
+    <div class="flex justify-center items-center py-4">
+      <iframe width="298" height="169" src="https://www.youtube.com/embed/n26NXNYbQG8"></iframe>
+      
+    </div>
+    <div class="grid grid-cols-3 ">
+        <img src="../img/lazy.png" alt="" />
+        <img src="../img/isaac.png" alt="" />
+        <img src="../img/ice.png" alt="" />
+        <img src="../img/boss.png" alt="" />
+        <img src="../img/kapi.png" alt="" />
+        <img src="../img/oliver.png" alt="" />
+        <img src="../img/fin.png" alt="" />
+        <img src="../img/sun.png" alt="" />
+        <img src="../img/kiw.png" alt="" />
+        <img src="../img/earth.png" alt="" />
+        <img src="../img/kasalong.png" alt="" />
+        <img src="../img/negan.png" alt="" />
+        <img src="../img/bar.png" alt="" />
+        <img src="../img/few.png" alt="" />
+        <img src="../img/daja.png" alt="" />
+        <div></div>
+        <img src="../img/Sorry.png" alt="" />
+      </div>
   </div>
+
 </template>
 
 <script setup>
-  // Any additional script can go here
+// Any additional script can go here
 </script>
 
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Italianno&family=Inter:wght@400;700&display=swap');
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap');
 
-.header-container {
-  background-color: #094037;
-  border-top: 4px solid #F3CB60;
-  padding: 20px;
-  text-align: center;
+body{
+  background-image:url(/img/main.png) ;
+  background-size: cover;
 }
 
-.header-title {
-  color: #F3CB60;
-  font-size: 3rem;
-  font-family: 'Italianno', cursive;
-}
 
-.nav-wrapper {
-  background-color: #F3CB60;
-  padding: 5px 10px; /* Reduce padding for a more compact design */
-  border-radius: 0 0 30px 30px;
-  margin-top: -20px;
-  width: 100%;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  justify-content: center;
-}
-
-.nav-links {
-  display: flex;
-  font-size: 8px;
-  justify-content: space-around;
-  gap: 5px; /* Reduce gap between links */
-}
-
-.nav-link {
-  color: #05705F;
-  padding: 8px 16px; /* Adjust padding for more compact links */
-  border-radius: 10px;
-  font-family: 'Inter', sans-serif;
-  font-weight: bold;
-  text-transform: uppercase;
-  transition: background-color 0.3s, color 0.3s;
-}
-
-.nav-link:hover {
-  background-color: #05705F;
-  color: #F3CB60;
+.day {
+  font-family: Kaushan Script;
+  color: white;
 }
 </style>
